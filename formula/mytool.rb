2ad0d21179f3ad7cd8529c69672623debe62218d
CharankumarSarlani <184059583+CharankumarSarlani@users.noreply.github.com>
2025-03-22 00:12:53 +0530

    Fix missing version field in MyTool formula

--- formula/mytool.rb
+++ formula/mytool.rb
@@ -2,7 +2,7 @@ class Mytool < Formula
   desc "DNA animation"
   homepage "https://github.com/CharankumarSarlani/brew-practice"
   url "https://github.com/CharankumarSarlani/brew-practice/releases/download/firstRelease/DNA-animation.tar"
-
+  version "1.0"
   sha256 "88c5e976541f183b0766208017fce6e0e7cab0f3793305409d9abc726ce5a20b"
   license "MIT"
 
