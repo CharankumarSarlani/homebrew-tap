bb34da449c0f77a3fc323fa2920566c2c6aea95b
CharankumarSarlani <184059583+CharankumarSarlani@users.noreply.github.com>
2025-03-21 23:56:17 +0530

    Adds dna animation formula

---
+++ formula/mytool.rb
@@ -0,0 +1,12 @@
+class Mytool < Formula
+  desc "DNA animation"
+  homepage "https://github.com/CharankumarSarlani/brew-practice"
+  url "https://github.com/CharankumarSarlani/brew-practice/releases/download/firstRelease/DNA-animation.tar"
+
+  sha256 "88c5e976541f183b0766208017fce6e0e7cab0f3793305409d9abc726ce5a20b"
+  license "MIT"
+
+  def install
+    bin.install "DNA-animation"
+  end
+end
